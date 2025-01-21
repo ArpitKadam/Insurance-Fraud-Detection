ff11231f017025cebd2d90510c1721c98fdda7c2
ArpitKadam <arpitkadam922@gmail.com>
2025-01-21 10:49:57 +0530

    changes

---
+++ src/Insurance_Fraud/utils/common.py
@@ -0,0 +1,8 @@
+import os
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Directory created: {path}")
+    else:
+        print(f"Directory already exists: {path}")
