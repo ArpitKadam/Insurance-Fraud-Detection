c791dec48c7f3d2f4e67c9680cdd142e316fabb6
Arpit Kadam <arpitkadam922@gmail.com>
2025-01-22 19:56:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="Insurance-Fraud-Detection",
-    version="0.0.1",
+    version="1.0.0",
     author="Arpit Kadam",
     author_email="arpitkadam2000@gmail.com",
     packages=find_packages(),
     python_requires=">=3.8",
-    install_requires=[]
+    install_requires=[],
+    include_package_data=True,
+    package_data={'': ['templates/*']}  
 )
